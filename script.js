854ecc17a26a910b5809ea1e7f80245ab352023f
Austin E <austinjjelliott@gmail.com>
2025-05-06 06:40:30 -0700

    Added game button to play 5 rounds on click

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const gameBtn = document.querySelector("#gameBtn");
+
 function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * 3);
   let choices = ["rock", "paper", "scissors"];
@@ -9,25 +11,7 @@ function getHumanChoice() {
   return choice.toLowerCase();
 }
 
-// function playRound(humanChoice, computerChoice) {
-//   if (humanChoice === computerChoice) {
-//     return `Human: ${humanChoice} vs Computer: ${computerChoice} It's a draw!`;
-//   }
-//   if (
-//     (humanChoice === "rock" && computerChoice === "scissors") ||
-//     (humanChoice === "paper" && computerChoice === "rock") ||
-//     (humanChoice === "scissors" && computerChoice === "paper")
-//   ) {
-//     humanScore++;
-//     return `Human: ${humanChoice} vs Computer: ${computerChoice} You Win!`;
-//   } else {
-//     computerScore++;
-//     return `Human: ${humanChoice} vs Computer: ${computerChoice} You Lose!`;
-//   }
-// }
-
-// playRound(humanSelection, computerSelection);
-function playGame() {
+gameBtn.addEventListener("click", () => {
   let humanScore = 0;
   let computerScore = 0;
   function playRound(humanChoice, computerChoice) {
@@ -55,4 +39,36 @@ function playGame() {
       `Round ${i}: ${result} Human Score: ${humanScore} Computer Score: ${computerScore}`
     );
   }
-}
+  console.log(`Final: Human: ${humanScore} vs Computer: ${computerScore}`);
+});
+
+// function playGame() {
+//   let humanScore = 0;
+//   let computerScore = 0;
+//   function playRound(humanChoice, computerChoice) {
+//     if (humanChoice === computerChoice) {
+//       return `Human: ${humanChoice} vs Computer: ${computerChoice} It's a draw!`;
+//     }
+//     if (
+//       (humanChoice === "rock" && computerChoice === "scissors") ||
+//       (humanChoice === "paper" && computerChoice === "rock") ||
+//       (humanChoice === "scissors" && computerChoice === "paper")
+//     ) {
+//       humanScore++;
+//       return `Human: ${humanChoice} vs Computer: ${computerChoice} You Win!`;
+//     } else {
+//       computerScore++;
+//       return `Human: ${humanChoice} vs Computer: ${computerChoice} You Lose!`;
+//     }
+//   }
+
+//   for (let i = 1; i <= 5; i++) {
+//     const humanSelection = getHumanChoice();
+//     const computerSelection = getComputerChoice();
+//     const result = playRound(humanSelection, computerSelection);
+//     console.log(
+//       `Round ${i}: ${result} Human Score: ${humanScore} Computer Score: ${computerScore}`
+//     );
+//   }
+//   return `Final: Human: ${humanScore} vs Computer: ${computerScore}`;
+// }
