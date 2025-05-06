1e5ea564ea1d19f3ab686319778bf6cd8d79eda0
Austin E <austinjjelliott@gmail.com>
2025-05-06 05:58:32 -0700

    created getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("linked");
+function getComputerChoice() {
+  let randomNum = Math.floor(Math.random() * 3);
+  let choices = ["rock", "paper", "scissors"];
+  return choices[randomNum];
+}
