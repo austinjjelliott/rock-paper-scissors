c0514dbdae82be9eeb6b7bc71fd8b79db9584d9f
Austin E <austinjjelliott@gmail.com>
2025-05-06 06:32:00 -0700

    added human choice, rounds, and playgame functions

--- script.js
+++ script.js
@@ -3,3 +3,56 @@ function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
   return choices[randomNum];
 }
+
+function getHumanChoice() {
+  let choice = prompt("Rock Paper Scissors?");
+  return choice.toLowerCase();
+}
+
+// function playRound(humanChoice, computerChoice) {
+//   if (humanChoice === computerChoice) {
+//     return `Human: ${humanChoice} vs Computer: ${computerChoice} It's a draw!`;
+//   }
+//   if (
+//     (humanChoice === "rock" && computerChoice === "scissors") ||
+//     (humanChoice === "paper" && computerChoice === "rock") ||
+//     (humanChoice === "scissors" && computerChoice === "paper")
+//   ) {
+//     humanScore++;
+//     return `Human: ${humanChoice} vs Computer: ${computerChoice} You Win!`;
+//   } else {
+//     computerScore++;
+//     return `Human: ${humanChoice} vs Computer: ${computerChoice} You Lose!`;
+//   }
+// }
+
+// playRound(humanSelection, computerSelection);
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      return `Human: ${humanChoice} vs Computer: ${computerChoice} It's a draw!`;
+    }
+    if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      humanScore++;
+      return `Human: ${humanChoice} vs Computer: ${computerChoice} You Win!`;
+    } else {
+      computerScore++;
+      return `Human: ${humanChoice} vs Computer: ${computerChoice} You Lose!`;
+    }
+  }
+
+  for (let i = 1; i <= 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    const result = playRound(humanSelection, computerSelection);
+    console.log(
+      `Round ${i}: ${result} Human Score: ${humanScore} Computer Score: ${computerScore}`
+    );
+  }
+}
